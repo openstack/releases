17ad4a9bd341a3fc14d83b58bac802692c537bc3
Doug Hellmann <doug@doughellmann.com>
2015-08-04 17:15:08 +0000

    if both tag and sha exist, make sure they match
    Update the validate command to compare the actual SHA of a tag with the
    one in the file to ensure they match.
    
    Change-Id: I5c04667cbb43cda548c3cc05a47d5a5830b32951

--- openstack_releases/gitutils.py
+++ openstack_releases/gitutils.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import os.path
 import subprocess
 
 import requests
@@ -36,18 +37,40 @@ def find_modified_deliverable_files():
     return filenames
 
 
-def commit_exists(repo, hash):
-    """Return boolean specifying whether the hash exists in the repository.
+def commit_exists(repo, ref):
+    """Return boolean specifying whether the reference exists in the repository.
 
     Uses a cgit query instead of looking locally to avoid cloning a
     repository or having Depends-On settings in a commit message allow
     someone to fool the check.
 
     """
-    url = CGIT_TEMPLATE % (repo, hash)
+    url = CGIT_TEMPLATE % (repo, ref)
     response = requests.get(url)
     missing_commit = (
         (response.status_code // 100 != 2)
         or 'Bad object id' in response.text
     )
     return not missing_commit
+
+
+def sha_for_tag(workdir, repo, version):
+    """Return the SHA for a given tag
+    """
+    # Check out the code.
+    subprocess.check_call(
+        ['zuul-cloner',
+         '--workspace', workdir,
+         'git://git.openstack.org',
+         repo]
+    )
+    # git log 2.3.11 -n 1 --pretty=format:%H
+    try:
+        actual_sha = subprocess.check_output(
+            ['git', 'log', str(version), '-n', '1', '--pretty=format:%H'],
+            cwd=os.path.join(workdir, repo),
+        )
+        actual_sha = actual_sha.strip()
+    except subprocess.CalledProcessError:
+        actual_sha = ''
+    return actual_sha
