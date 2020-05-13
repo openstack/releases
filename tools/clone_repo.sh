62c2fc43176363f8738effef20b72468d5190e34
Sean McGinnis <sean.mcginnis@gmail.com>
2020-05-13 05:58:59 -0500

    Use updated git cache dir default path
    Change-Id: I7c3ed1e08c7ef1a62845df0d9c5b22f400d8d90b
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- tools/clone_repo.sh
+++ tools/clone_repo.sh
@@ -145,7 +145,7 @@ fi
 
 set -e
 
-cache_remote="$CACHE_DIR/$REPO"
+cache_remote="$CACHE_DIR/opendev.org/$REPO"
 if [ ! -d "$cache_remote" ]; then
     echo "WARNING: Cache directory $cache_remote does not exist, ignoring."
     cache_remote=""
